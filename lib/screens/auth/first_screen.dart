0b8f118ba49fbef1516fe5e61a2f44a7b867d1c1
Samvabya Sarkar <samvabyat1@gmail.com>
2025-01-09 23:52:35 +0530

    commit 9

--- lib/screens/auth/first_screen.dart
+++ lib/screens/auth/first_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,9 +21,165 @@ class FirstScreen extends StatefulWidget {
 class _FirstScreenState extends State<FirstScreen> {
   final _controller = PageController();
   int pgIndex = 0;
+  var hovercolorweb = Colors.transparent;
+  var hoverborderspread = 10.0;
 
   @override
   Widget build(BuildContext context) {
+    if (AppSizing.getWidth(context) > 600)
+      return Scaffold(
+          body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.network(
+              'https://images.unsplash.com/photo-1731575115709-d4325615e868?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Container(
+              height: double.infinity,
+              constraints: BoxConstraints(maxWidth: 1000),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    'All I Dreamed Of Is Your Eyes',
+                    style: GoogleFonts.permanentMarker(
+                      shadows: [
+                        Shadow(
+                          color: Colors.black,
+                          offset: Offset(0, 5),
+                        )
+                      ],
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    width: double.maxFinite,
+                    height: AppSizing.getHeight(context) * 0.3,
+                    child: DefaultTextStyle(
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.nothingYouCouldDo(
+                        shadows: [
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(0, 0),
+                            blurRadius: 30,
+                          )
+                        ],
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      child: AnimatedTextKit(
+                        repeatForever: true,
+                        animatedTexts: [
+                          RotateAnimatedText(
+                              "They say You know when you know So let's face it, you had me at hello Hesitation never helps How could this be anything, anything else?"),
+                          RotateAnimatedText(
+                              "When All I dream of is your eyes All I long for is your touch And darling, something tells me that's enough, mmm You can say that I'm a fool And I don't know very much, but I Think They Call This Love"),
+                          RotateAnimatedText(
+                              "One smile, one kiss, two lonely hearts is all that it takes Now baby, you're on my mind, every night, every day Good vibrations getting loud How could this be anything, anything else?"),
+                        ],
+                        onTap: () {
+                          print("Tap Event");
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    splashColor: Colors.redAccent,
+                    enableFeedback: true,
+                    onTap: () async {
+                      UserCredential? cred;
+                      try {
+                        cred = await signInWithGoogle();
+                        await checkHasAccount(cred.user!.email ?? '')
+                            ? Navigator.pushReplacement(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => NavScreen(),
+                                ))
+                            : Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => SecondScreen(
+                                    cred: cred,
+                                  ),
+                                ));
+                      } catch (e) {
+                        showSnack(context, 'Failed: ${e.toString()}');
+                        debugPrint(e.toString());
+                      }
+                    },
+                    onHover: (value) => setState(() {
+                      hovercolorweb =
+                          value ? Colors.redAccent : Colors.transparent;
+
+                      hoverborderspread = value ? 30.0 : 10.0;
+                    }),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: hovercolorweb,
+                          border: Border.all(
+                            color: Colors.redAccent,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                          boxShadow: [
+                            BoxShadow(
+                              blurStyle: BlurStyle.outer,
+                              color: Colors.redAccent,
+                              offset: Offset(0, 0),
+                              blurRadius: hoverborderspread,
+                            )
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: Wrap(
+                          spacing: 10,
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/google.png',
+                              width: 16,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Sign in with Google',
+                              style: GoogleFonts.poppins(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ).animate().fadeIn(
+                          delay: Duration(milliseconds: 700),
+                          curve: Curves.fastOutSlowIn,
+                          duration: Duration(milliseconds: 1000),
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ));
+
     return Scaffold(
       body: Stack(
         alignment: Alignment.bottomCenter,
